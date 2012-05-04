1701e6af1ee9f4d2ff3bf790cda8d4e955701f3e
simonetripodi <simone.tripodi@gmail.com>
2012-05-04 21:19:03 +0200

    Wagon: Deploying site to repository

---
+++ apidocs/assets/navtree_data.js
@@ -0,0 +1,23 @@
+var NAVTREE_DATA =
+[ [ "org.nnsoft.guice.junice", "org/nnsoft/guice/junice/package-summary.html", [ [ "Description", "org/nnsoft/guice/junice/package-descr.html", null, "" ], [ "Classes", null, [ [ "GuiceMockModule", "org/nnsoft/guice/junice/GuiceMockModule.html", null, "" ], [ "JUniceRunner", "org/nnsoft/guice/junice/JUniceRunner.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.nnsoft.guice.junice.annotation", "org/nnsoft/guice/junice/annotation/package-summary.html", [ [ "Description", "org/nnsoft/guice/junice/annotation/package-descr.html", null, "" ], [ "Classes", null, [ [ "Mock.NoAnnotation", "org/nnsoft/guice/junice/annotation/Mock.NoAnnotation.html", null, "" ] ]
+, "" ], [ "Annotations", null, [ [ "GuiceModules", "org/nnsoft/guice/junice/annotation/GuiceModules.html", null, "" ], [ "GuiceProvidedModules", "org/nnsoft/guice/junice/annotation/GuiceProvidedModules.html", null, "" ], [ "Mock", "org/nnsoft/guice/junice/annotation/Mock.html", null, "" ], [ "MockFramework", "org/nnsoft/guice/junice/annotation/MockFramework.html", null, "" ] ]
+, "" ], [ "Enums", null, [ [ "MockObjType", "org/nnsoft/guice/junice/annotation/MockObjType.html", null, "" ], [ "MockType", "org/nnsoft/guice/junice/annotation/MockType.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.nnsoft.guice.junice.handler", "org/nnsoft/guice/junice/handler/package-summary.html", [ [ "Description", "org/nnsoft/guice/junice/handler/package-descr.html", null, "" ], [ "Classes", null, [ [ "GuiceInjectableClassHandler", "org/nnsoft/guice/junice/handler/GuiceInjectableClassHandler.html", null, "" ], [ "GuiceModuleHandler", "org/nnsoft/guice/junice/handler/GuiceModuleHandler.html", null, "" ], [ "GuiceProvidedModuleHandler", "org/nnsoft/guice/junice/handler/GuiceProvidedModuleHandler.html", null, "" ], [ "MockFrameworkHandler", "org/nnsoft/guice/junice/handler/MockFrameworkHandler.html", null, "" ], [ "MockHandler", "org/nnsoft/guice/junice/handler/MockHandler.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.nnsoft.guice.junice.mock", "org/nnsoft/guice/junice/mock/package-summary.html", [ [ "Description", "org/nnsoft/guice/junice/mock/package-descr.html", null, "" ], [ "Interfaces", null, [ [ "MockEngine", "org/nnsoft/guice/junice/mock/MockEngine.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.nnsoft.guice.junice.mock.framework", "org/nnsoft/guice/junice/mock/framework/package-summary.html", [ [ "Description", "org/nnsoft/guice/junice/mock/framework/package-descr.html", null, "" ], [ "Classes", null, [ [ "EasyMockFramework", "org/nnsoft/guice/junice/mock/framework/EasyMockFramework.html", null, "" ], [ "MockitoFramework", "org/nnsoft/guice/junice/mock/framework/MockitoFramework.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.nnsoft.guice.junice.mock.guice", "org/nnsoft/guice/junice/mock/guice/package-summary.html", [ [ "Description", "org/nnsoft/guice/junice/mock/guice/package-descr.html", null, "" ], [ "Classes", null, [ [ "MockMembersInjector", "org/nnsoft/guice/junice/mock/guice/MockMembersInjector.html", null, "" ], [ "MockTypeListener", "org/nnsoft/guice/junice/mock/guice/MockTypeListener.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.nnsoft.guice.junice.reflection", "org/nnsoft/guice/junice/reflection/package-summary.html", [ [ "Description", "org/nnsoft/guice/junice/reflection/package-descr.html", null, "" ], [ "Interfaces", null, [ [ "AnnotationHandler", "org/nnsoft/guice/junice/reflection/AnnotationHandler.html", null, "" ], [ "ClassHandler", "org/nnsoft/guice/junice/reflection/ClassHandler.html", null, "" ], [ "FieldHandler", "org/nnsoft/guice/junice/reflection/FieldHandler.html", null, "" ], [ "MethodHandler", "org/nnsoft/guice/junice/reflection/MethodHandler.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "ClassVisitor", "org/nnsoft/guice/junice/reflection/ClassVisitor.html", null, "" ] ]
+, "" ], [ "Exceptions", null, [ [ "HandleException", "org/nnsoft/guice/junice/reflection/HandleException.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
